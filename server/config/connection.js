e015bfd77f7da1763c3d734bef0e4f44d815ac6e
Yixuan Sally Zhang <sallyhoney96@gmail.com>
2022-06-02 20:24:11 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,12 +2,13 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  'mongodb://localhost:27107/book-search-engine-googleapi', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
+
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   } 
 );
 
